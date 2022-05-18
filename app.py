e34a339fdedfc8a1252d8ebb7b88ccdb76f8997e
Joakim Wassberg <joakim@arthead.se>
2022-05-18 09:16:58 +0200

    Fixed linter errors

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, Response, request
+"""
+Main Api module
+"""
+
 import json
+from flask import Flask, Response, request
 from db import users
 
 
 def create_app():
+    """
+    Flask factory function
+    :return: Flask app object
+    """
     app = Flask(__name__)
 
     @app.get('/api/v1.0/users')
